b5f531ba39e6c482286a70a9c4ad305ee350395d
Станислав <__email@example.com>
2025-05-07 10:39:35 +0300

    удалил

--- app/models/Base.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Column,Integer
-
-class Base(DeclarativeBase):
-    id = Column(Integer, primary_key=True, autoincrement=True, unique=True)
-    
-
--- app/schemas/Finance.py
+++
@@ -1,9 +0,0 @@
-from typing import Literal
-from pydantic import BaseModel
-
-
-class Finance(BaseModel):
-     
-     Description:str
-     
-     type_transaction:Literal["income","expense"]
